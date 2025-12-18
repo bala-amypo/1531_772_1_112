305ece5ca893344465145b7c6f3da68ea019582d
bala-amypo <bala@amypo.in>
2025-12-18 18:08:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/Entity/VerificationRuleEntity.java
@@ -0,0 +1,72 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class VerificationRuleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ruleName;
+    private String ruleDescription;
+    private String ruleType;
+    private Boolean active;
+
+    public VerificationRuleEntity() {
+    }
+
+    public VerificationRuleEntity(Long id, String ruleName,
+                                  String ruleDescription,
+                                  String ruleType, Boolean active) {
+        this.id = id;
+        this.ruleName = ruleName;
+        this.ruleDescription = ruleDescription;
+        this.ruleType = ruleType;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public String getRuleDescription() {
+        return ruleDescription;
+    }
+
+    public void setRuleDescription(String ruleDescription) {
+        this.ruleDescription = ruleDescription;
+    }
+
+    public String getRuleType() {
+        return ruleType;
+    }
+
+    public void setRuleType(String ruleType) {
+        this.ruleType = ruleType;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
