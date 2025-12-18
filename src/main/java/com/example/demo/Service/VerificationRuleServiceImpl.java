8c52d185b4366d22c14da86030f549c9b737636f
bala-amypo <bala@amypo.in>
2025-12-18 18:14:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/Service/VerificationRuleServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.demo.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.VerificationRuleEntity;
+import com.example.demo.Repository.VerificationRuleRepository;
+
+@Service
+public class VerificationRuleServiceImpl implements VerificationRuleService {
+
+    @Autowired
+    VerificationRuleRepository verificationRuleRepo;
+
+    @Override
+    public VerificationRuleEntity createVerificationRule(
+            VerificationRuleEntity verificationRuleEntity) {
+
+        return verificationRuleRepo.save(verificationRuleEntity);
+    }
+
+    @Override
+    public List<VerificationRuleEntity> getAllVerificationRules() {
+
+        return verificationRuleRepo.findAll();
+    }
+}
