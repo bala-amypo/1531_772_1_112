ff769c2bfccf509401ae901841a82d40baf2b70e
bala-amypo <bala@amypo.in>
2025-12-19 03:54:44 +0000

    User commit

--- src/main/java/com/example/demo/Controller/AuditTrailController.java
+++ src/main/java/com/example/demo/Controller/AuditTrailController.java
@@ -1,32 +1,33 @@
  package com.example.demo.Controller;
 
-import java.util.List;
+ import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
+ import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.Entity.AuditTrailRecordEntity;
-import com.example.demo.Service.AuditTrailService;
+ import com.example.demo.Entity.AuditTrailRecordEntity;
+ import com.example.demo.Service.AuditTrailService;
 
-@RestController
-public class AuditTrailController {
+ @RestController
+ public class AuditTrailController {
 
-    @Autowired
-    private AuditTrailService auditTrailService;
+     @Autowired
+         private AuditTrailService auditTrailService;
 
-    @PostMapping("/addaudittrail")
-    public AuditTrailRecordEntity addAuditTrail(
-            @RequestBody AuditTrailRecordEntity auditTrailRecord) {
+             @PostMapping("/addaudittrail")
+                 public AuditTrailRecordEntity addAuditTrail(
+                             @RequestBody AuditTrailRecordEntity auditTrailRecord) {
 
-        return auditTrailService.createAuditTrail(auditTrailRecord);
-    }
+                                     return auditTrailService.createAuditTrail(auditTrailRecord);
+                                         }
 
-    @GetMapping("/showaudittrail")
-    public List<AuditTrailRecordEntity> getAllAuditTrails() {
+                                             @GetMapping("/showaudittrail")
+                                                 public List<AuditTrailRecordEntity> getAllAuditTrails() {
 
-        return auditTrailService.getAllAuditTrails();
-    }
-}
+                                                         return auditTrailService.getAllAuditTrails();
+                                                             }
+                                                             }
+                                                             
