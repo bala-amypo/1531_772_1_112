9f75b87852923b3f5d5e56d6afe715434d82d204
bala-amypo <bala@amypo.in>
2025-12-26 04:59:05 +0000

    User commit

--- src/main/java/com/example/demo/Entity/UserEntity.java
+++ src/main/java/com/example/demo/Entity/UserEntity.java
@@ -1,83 +1,14 @@
-package com.example.demo.Entity;
+package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
 
 @Entity
-public class UserEntity {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
-    
-    private String email;
     private String password;
-    private LocalDateTime createdDate;
-    private LocalDateTime updatedDate;
-
-
-    public UserEntity(Long id, String username, String email, String password, LocalDateTime createdDate, LocalDateTime updatedDate) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.createdDate = createdDate;
-        this.updatedDate = updatedDate;
-    }
-    public UserEntity() {
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(LocalDateTime updatedDate) {
-        this.updatedDate = updatedDate;
-    }
 }
--- src/main/java/com/example/demo/Repository/UserRepository.java
+++ src/main/java/com/example/demo/Repository/UserRepository.java
@@ -1,14 +1,7 @@
-package com.example.demo.Repository;
-
-import java.util.Optional;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.User;
 
-import com.example.demo.Entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-    Optional<UserEntity> findByUsername(String username);
-
-    Optional<UserEntity> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
 }
