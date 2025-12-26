879a05279b617d47ffa018a8c943b7815f7576de
bala-amypo <bala@amypo.in>
2025-12-26 10:01:17 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,20 @@
 package com.example.demo.service;
 
 import java.util.List;
-import com.example.demo.Entity.UserEntity;
+import com.example.demo.entity.UserEntity;
 
 public interface UserService {
 
     UserEntity createUser(UserEntity user);
 
     List<UserEntity> getAllUsers();
+
+    UserEntity getUserById(Long id);
+
+    UserEntity getUserByUsername(String username);
+
+    UserEntity getUserByEmail(String email);
+
+    void deleteUser(Long id);
 }
+
--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import com.example.demo.entity.UserEntity;
+import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -23,22 +24,22 @@ public class UserServiceImpl implements UserService {
         return userRepo.findAll();
     }
 
-    
+    @Override
     public UserEntity getUserById(Long id) {
         return userRepo.findById(id).orElse(null);
     }
 
-    
+    @Override
     public UserEntity getUserByUsername(String username) {
         return userRepo.findByUsername(username).orElse(null);
     }
 
-    
+    @Override
     public UserEntity getUserByEmail(String email) {
         return userRepo.findByEmail(email).orElse(null);
     }
 
-    
+    @Override
     public void deleteUser(Long id) {
         userRepo.deleteById(id);
     }
