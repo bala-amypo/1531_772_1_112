5fd5add9712a09d4f9e3f9745be021411aa9a067
bala-amypo <bala@amypo.in>
2025-12-26 10:42:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -6,7 +6,8 @@ import jakarta.persistence.*;
 @Table(name = "users")
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Gener
+        @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long id;}@Table(name = "users")public class UserEntity {   atedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
 
