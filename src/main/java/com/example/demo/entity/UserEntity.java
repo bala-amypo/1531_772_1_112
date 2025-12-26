cbd29e628831b49915a0cc44008a61be0d5bbc0d
bala-amypo <bala@amypo.in>
2025-12-26 13:29:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,13 +1,16 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "users")
-public class UserEntity {
-    @Id
-    @Gener
-        @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long id;}@Table(name = "users")public class UserEntity {   atedValue(strategy = GenerationType.IDENTITY)
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String fullName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role = "VIEWER";
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
-
