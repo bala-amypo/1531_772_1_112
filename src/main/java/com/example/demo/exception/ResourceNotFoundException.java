6396246796c6babc0418819671c3eeb69b004b06
bala-amypo <bala@amypo.in>
2025-12-26 14:05:22 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,9 +1,15 @@
-package com.example.demo.Execption;
+package com.example.demo.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Runtime exception used when a requested resource (such as user or holder) is missing.
+ * Maps to HTTP 404 Not Found.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
-
-    
 }
